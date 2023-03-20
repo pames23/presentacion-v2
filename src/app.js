278854c010b6aae9a732df9535e8cbc3520816d3
pames23 <pameschaparro@gmail.com>
2023-03-20 20:27:46 -0300

    primera version

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('Hola mundo!!!');
+const express = require ('express')
+const app = express();
+const PORT = 3001;
+app.get ('/api, (req,res)=> {
+    res.json ({mensaje:'hola mundo'}).status(200)
+});
+app.listen(PORT, ()=>{console.log('estoy escuchando en el puerto:' +PORT)})
